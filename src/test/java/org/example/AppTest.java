fded7c211dd35f4b73c9b58696f6c8889c4006a7
v-forestai <forestaiste@hotmail.com>
2021-01-20 18:49:35 +0800

    Sudoku

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -107,4 +107,22 @@ public class AppTest {
 
         System.out.println(containerWithMostWater.maxArea(nums));
     }
+
+    @Test
+    public void validSudokuTest() {
+        ValidSudoku validSudoku = new ValidSudoku();
+        char[][] board =
+                {
+                        {'5','3','.','.','7','.','.','.','.'},
+                        {'6','.','.','1','9','5','.','.','.'},
+                        {'.','9','8','.','.','.','.','6','.'},
+                        {'8','.','.','.','6','.','.','.','3'},
+                        {'4','.','.','8','.','3','.','.','1'},
+                        {'7','.','.','.','2','.','.','.','6'},
+                        {'.','6','.','.','.','.','2','8','.'},
+                        {'.','.','.','4','1','9','.','.','5'},
+                        {'.','.','.','.','8','.','.','7','9'},
+                };
+        System.out.println(validSudoku.isValidSudoku(board));
+    }
 }
