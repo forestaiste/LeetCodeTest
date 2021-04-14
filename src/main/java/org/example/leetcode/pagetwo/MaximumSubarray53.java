c9d4e6ba1c8ac3c0c47993792afc393d85f318fe
v-forestai <forestaiste@hotmail.com>
2021-04-14 22:14:52 +0800

    IsSubsequence392

--- src/main/java/org/example/leetcode/pagetwo/MaximumSubarray53.java
+++ src/main/java/org/example/leetcode/pagetwo/MaximumSubarray53.java
@@ -40,4 +40,19 @@ public class MaximumSubarray53 {
 
         return result;
     }
+
+    public int maxSubArray2(int[] nums) {
+        int n = nums.length;
+
+        int[] f = new int[n];
+        f[0] = nums[0];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n; i++) {
+            f[i] = Math.max(f[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, f[i]);
+        }
+
+        return max;
+    }
 }
