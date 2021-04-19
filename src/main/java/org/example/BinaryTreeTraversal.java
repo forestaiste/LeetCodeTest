1653041f377487fea3a528a56ac03bf2c0d85334
v-forestai <forestaiste@hotmail.com>
2021-04-19 22:35:58 +0800

    SymmetricTree101

---
+++ src/main/java/org/example/BinaryTreeTraversal.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import org.example.models.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeTraversal {
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.right != null)
+                stack.push(node.left);
+        }
+
+        return result;
+    }
+
+    public List<Integer> inOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            else {
+                TreeNode node = stack.pop();
+                result.add(node.val);
+                current = node.right;
+            }
+        }
+
+        return result;
+    }
+
+    public List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            result.add(0, node.val);
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/example/leetcode/pagethree/SymmetricTree101.java
@@ -0,0 +1,9 @@
+package org.example.leetcode.pagethree;
+
+import org.example.models.TreeNode;
+
+public class SymmetricTree101 {
+    public boolean isSymmetric(TreeNode root) {
+        return true;
+    }
+}
