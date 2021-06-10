8b87a6b09173286120816ee0ff697b018161072c
v-forestai <forestaiste@hotmail.com>
2021-06-10 18:32:04 +0800

    BasicCalculatorII227

---
+++ src/main/java/org/example/leetcode/pagefive/BasicCalculatorII227.java
@@ -0,0 +1,63 @@
+package org.example.leetcode.pagefive;
+
+//227. 基本计算器 II
+//        给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+//
+//        整数除法仅保留整数部分。
+//
+//        示例 1：
+//
+//        输入：s = "3+2*2"
+//        输出：7
+//        示例 2：
+//
+//        输入：s = " 3/2 "
+//        输出：1
+//        示例 3：
+//
+//        输入：s = " 3+5 / 2 "
+//        输出：5
+//
+//
+//        提示：
+//
+//        1 <= s.length <= 3 * 105
+//        s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+//        s 表示一个 有效表达式
+//        表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+//        题目数据保证答案是一个 32-bit 整数
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BasicCalculatorII227 {
+    public int calculate(String s) {
+        Queue<Integer> dataQueue = new LinkedList<>();
+
+        Queue<Character> operatorQueue = new LinkedList<>();
+
+        StringBuilder number = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ') {
+                if (number.length() > 0) {
+                    dataQueue.add(Integer.parseInt(number.toString()));
+                    number = new StringBuilder();
+                }
+            }
+            else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                operatorQueue.add(s.charAt(i));
+
+                if (number.length() > 0) {
+                    dataQueue.add(Integer.parseInt(number.toString()));
+                    number = new StringBuilder();
+                }
+            }
+            else {
+                number.append(c);
+            }
+
+        }
+    }
+}
