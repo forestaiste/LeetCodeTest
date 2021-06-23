d8a09b7718d8231b7b6b9b6006f01ef9c61c9e4f
v-forestai <forestaiste@hotmail.com>
2021-06-23 18:36:16 +0800

    QuickSort

--- src/test/java/org/example/GeneralTest.java
+++ src/test/java/org/example/GeneralTest.java
@@ -2,7 +2,18 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class GeneralTest {
 
+    @Test
+    public void quickSortTest() {
+        QuickSort quickSort = new QuickSort();
+        int[] nums = {5, 9, 2, 8, 10, 3, 4};
+        quickSort.sort(nums);
+//        quickSort.sort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
 
+
+    }
 }
