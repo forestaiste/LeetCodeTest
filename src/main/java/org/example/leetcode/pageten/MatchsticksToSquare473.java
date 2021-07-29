997842ff741e7adfef3ba278001e8508fced66a5
v-forestai <forestaiste@hotmail.com>
2021-07-29 21:42:12 +0800

    Experience.txt

--- src/main/java/org/example/leetcode/pageten/MatchsticksToSquare473.java
+++ src/main/java/org/example/leetcode/pageten/MatchsticksToSquare473.java
@@ -64,49 +64,50 @@ public class MatchsticksToSquare473 {
         return false;
     }
 
-    public boolean makesquare(int[] matchsticks) {
-        int sum = Arrays.stream(matchsticks).sum();
-
-        if (sum % 4 != 0)
+    public boolean makesquare(int[] nums) {
+        int total = 0;
+        //统计所有火柴的长度
+        for (int num : nums) {
+            total += num;
+        }
+        //如果所有火柴的长度不是4的倍数，直接返回false
+        if (total == 0 || (total & 3) != 0)
             return false;
-
-        sums = new int[4];
-        side = sum / 4;
-        Arrays.sort(matchsticks);
-
-        int length = matchsticks.length;
-        boolean[] occupied = new boolean[side];
-
-        for (int i = 0; i < side; i++) {
-            occupied[i] = false;
-        }
-
-        int[] f = new int[side + 1];
-        for (int i = 0; i < 4; i++) {
-            Arrays.fill(f, 0);
-            for (int j = 0; j < length; j++) {
-                for (int k = side; k >= matchsticks[j]; k--) {
-                    if (!occupied[j] && f[j] < side) {
-                        f[k] = f[k - matchsticks[j]] + matchsticks[j];
-                        sums[i] = f[k];
-                    }
-
-                }
-
-
-            }
-
-            if (sums[i] == side)
-                break;
-            else
-                return false;
-        }
-
-        return true;
+        //回溯
+        return backtrack(nums, 0, total >> 2, new int[4]);
     }
 
+    //index表示访问到当前火柴的位置，target表示正方形的边长，size是长度为4的数组，
+    //分别保存正方形4个边的长度
+    private boolean backtrack(int[] nums, int index, int target, int[] size) {
+        if (index == nums.length) {
+            //如果火柴都访问完了，并且size的4个边的长度都相等，说明是正方形，直接返回true，
+            //否则返回false
+            if (size[0] == size[1] && size[1] == size[2] && size[2] == size[3])
+                return true;
+            return false;
+        }
+        //到这一步说明火柴还没访问完
+        for (int i = 0; i < size.length; i++) {
+            //如果把当前火柴放到size[i]这个边上，他的长度大于target，我们直接跳过
+            if (size[i] + nums[index] > target)
+                continue;
+            //如果当前火柴放到size[i]这个边上，长度不大于target，我们就放上面
+            size[i] += nums[index];
+            //然后在放下一个火柴，如果最终能变成正方形，直接返回true
+            if (backtrack(nums, index + 1, target, size))
+                return true;
+            //如果当前火柴放到size[i]这个边上，最终不能构成正方形，我们就把他从
+            //size[i]这个边上给移除，然后在试其他的边
+            size[i] -= nums[index];
+        }
+        //如果不能构成正方形，直接返回false
+        return false;
+    }
+
+
     public static void main(String[] args) {
-        int[] matchsticks = {1,1,1,1,2,2,2,2,3,3,3,3};
+        int[] matchsticks = {1,2,1,2,2};
 
         MatchsticksToSquare473 matchsticksToSquare = new MatchsticksToSquare473();
 
